db09f7755390faa561ac6181c236c99c373453c0
sheikhubawa001 <aftabsheikh2210@gmail.com>
2026-02-07 09:30:35 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,17 @@
-install
+#!/bin/bash
+clear
+echo "Installing aftab tool..."
+sleep 1
+
+chmod +x aftab
+
+if [ -d "$PREFIX/bin" ]; then
+  mv aftab $PREFIX/bin/
+else
+  echo "PREFIX/bin not found"
+  exit 1
+fi
+
+echo ""
+echo "Install complete ✅"
+echo "Now type: aftab"
